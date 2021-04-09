b0c0d0937e26ddd127fb2ab2c89e0ac6997d6ccc
Chantz-J <johnson.chantz@gmail.com>
2021-04-09 17:23:14 -0400

    Action api complete

--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,65 @@
 // Write your "actions" router here!
+const express = require('express')
+const router = express.Router()
+
+const Actions = require('./actions-model')
+const mw = require('../middleware/actions-middleware')
+
+//[GET]
+router.get('/', (req, res) => {
+    Actions.get()
+        .then(action => {
+            res.status(200).json(action);
+        })
+        .catch(err => {
+            res.status(404).json({ message: 'Action not found.' });
+        })
+})
+
+
+//[POST]
+router.post('/', mw.checkAction, async (req, res, next) => {
+    try {
+        const newAction = req.body;
+        const data = await Actions.insert(newAction);
+        res.status(200).json(data);
+    } catch (err) {
+        next(err);
+    }
+});
+
+//[UPDATE]
+router.put('/:id', mw.checkId, mw.checkActionUpdates, (req, res, next) => {
+    const id = req.params.id;
+    const changes = req.body;
+
+    !changes && res.status(400).json({ message: "No updates in request body." })
+
+    Actions.update(id, changes)
+        .then(update => {
+            res.status(200).json(update);
+        })
+        .catch(err => {
+            next();
+        })
+})
+
+//[DELETE]
+router.delete('/:id', mw.checkId, async (req, res, next) => {
+    try {
+        const id = req.params.id;
+        const data = await Actions.remove(id);
+        res.json(data);
+    } catch (err) {
+        next(err);
+    }
+})
+
+
+router.use((error, req, res) => {
+    res
+      .status(500)
+      .json({ message: error.message, stack: error.stack });
+  });
+
+module.exports = router
--- api/server.js
+++ api/server.js
@@ -4,6 +4,7 @@ const helmet = require('helmet')
 const server = express();
 
 const ProjectRouter = require('./projects/projects-router')
+const ActionRouter = require('./actions/actions-router')
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
 server.use(express.json())
@@ -12,6 +13,7 @@ server.use(helmet())
 
 //ENDPOINTS
 server.use('/api/projects', ProjectRouter)
+server.use('/api/actions', ActionRouter)
 
 server.get('/', (req, res) => {
     res.send(`
