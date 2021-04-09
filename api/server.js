caedeec9247ab2ae8bea9c720d014e189e9e24c9
Chantz-J <johnson.chantz@gmail.com>
2021-04-09 04:31:00 -0400

    First commit. Added libraries to srver. Created scripts.

--- api/server.js
+++ api/server.js
@@ -1,7 +1,20 @@
-const express = require('express');
+const express = require('express')
+const morgan = require('morgan')
+const helmet = require('helmet')
 const server = express();
+
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
+server.use(express.json())
+server.use(morgan('dev'))
+server.use(helmet())
+
+server.get('/', (req, res) => {
+    res.send(`
+    <h1>Sprint Challenge Server</h1>
+    <p>Welcome my Sprint Challenge Api! ^_^</p>
+    `)
+})
 
 module.exports = server;
